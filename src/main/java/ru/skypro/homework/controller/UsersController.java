439bb9e3306607400da99e56faac5cb0aad6a400
Stanislav <stdu92@mail.ru>
2023-08-07 19:06:11 +0300

    Добавил пустые методы без аргументов и блока кода

--- src/main/java/ru/skypro/homework/controller/UsersController.java
+++ src/main/java/ru/skypro/homework/controller/UsersController.java
@@ -2,9 +2,13 @@ package ru.skypro.homework.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.skypro.homework.dto.Login;
+import ru.skypro.homework.dto.NewPassword;
+import ru.skypro.homework.dto.Register;
+import ru.skypro.homework.dto.UpdateUser;
+
 
 @Slf4j
 @CrossOrigin(value = "http://localhost:3000")
@@ -14,26 +18,24 @@ import ru.skypro.homework.dto.Login;
 public class UsersController {
 
 
-
-
     @PostMapping("/set_password")
-    public ResponseEntity<?> setPassword(@RequestBody ){
-        return null;
+    public ResponseEntity<?> setPassword(@RequestBody NewPassword newPassword){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
     @GetMapping("/me")
-    public ResponseEntity<?> getUser(){
-        return null;
+    public ResponseEntity<Register> getUser(){
+        return ResponseEntity.ok(new Register());
     }
 
     @PatchMapping("/me")
-    public ResponseEntity<?> updateUser(@RequestBody ){
-        return null;
+    public ResponseEntity<?> updateUser(@RequestBody UpdateUser updateUser){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PatchMapping("/me/image")
-    public ResponseEntity<?> updateUserImage(@RequestBody ){
-        return null;
+    public ResponseEntity<?> updateUserImage(@RequestBody byte[] image){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
