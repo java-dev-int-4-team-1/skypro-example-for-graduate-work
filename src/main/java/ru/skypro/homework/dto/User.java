d2ffc954d1b2aa94c4ccc12b187e3e7ce51ab462
Stanislav <stdu92@mail.ru>
2023-08-07 20:36:01 +0300

    Добавил пустые методы без аргументов и блока кода

--- src/main/java/ru/skypro/homework/dto/User.java
+++ src/main/java/ru/skypro/homework/dto/User.java
@@ -3,10 +3,12 @@ package ru.skypro.homework.dto;
 import lombok.Data;
 
 @Data
-public class UpdateUser {
-
+public class User {
+    private long id;
+    private String email;
     private String firstName;
     private String lastName;
     private String phone;
-
+    private Role role;
+    private byte[] image;
 }
