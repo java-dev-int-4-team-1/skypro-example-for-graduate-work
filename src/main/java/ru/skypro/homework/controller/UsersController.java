30ab8d6f99c416df7d8c7b98d5bf3c7932053d97
Stanislav <stdu92@mail.ru>
2023-08-08 21:59:26 +0300

    Обновил пустые

--- src/main/java/ru/skypro/homework/controller/UsersController.java
+++ src/main/java/ru/skypro/homework/controller/UsersController.java
@@ -1,23 +1,16 @@
 package ru.skypro.homework.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.NewPassword;
-import ru.skypro.homework.dto.Register;
 import ru.skypro.homework.dto.UpdateUser;
 import ru.skypro.homework.dto.User;
-
-import java.awt.*;
 
 
 @Slf4j
@@ -27,107 +20,24 @@ import java.awt.*;
 @RequestMapping("/users")
 public class UsersController {
 
-    @Operation(
-            summary = "Обновление пароля",
-            operationId = "setPassword",
-            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = NewPassword.class)
-                    )
-            ),
-            responses = {@ApiResponse(
-                    responseCode = "200",
-                    description = "OK"
-            ),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized"
-                    ),
-                    @ApiResponse(
-                            responseCode = "403",
-                            description = "Forbidden"
-                    )},
-            tags = "Пользователи"
-    )
+
     @PostMapping("/set_password")
     public ResponseEntity<?> setPassword(@RequestBody NewPassword newPassword) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-
-    @Operation(
-            summary = "Получение информации об авторизованном пользователе",
-            operationId = "getUser",
-            responses = {@ApiResponse(
-                    responseCode = "200",
-                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = User.class)
-                    ),
-                    description = "OK"
-            ),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized"
-                    )},
-            tags = "Пользователи"
-    )
     @GetMapping("/me")
     public ResponseEntity<User> getUser() {
         return ResponseEntity.ok(new User());
     }
 
-
-    @Operation(
-            summary = "Обновление информации об авторизованном пользователе",
-            operationId = "updateUser",
-            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = UpdateUser.class)
-                    )
-            ),
-            responses = {@ApiResponse(
-                    responseCode = "200",
-                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            schema = @Schema(implementation = UpdateUser.class)
-                    ),
-                    description = "OK"
-            ),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized"
-                    )},
-            tags = "Пользователи"
-    )
     @PatchMapping("/me")
     public ResponseEntity<?> updateUser(@RequestBody UpdateUser updateUser) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-
-    @Operation(
-            summary = "Обновление аватара авторизованного пользователя",
-            operationId = "updateUserImage",
-            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                    content = @Content(
-                            mediaType = MediaType.MULTIPART_FORM_DATA_VALUE
-                    )
-            ),
-
-            responses = {@ApiResponse(
-                    responseCode = "200",
-                    description = "OK"
-            ),
-                    @ApiResponse(
-                            responseCode = "401",
-                            description = "Unauthorized"
-                    )},
-            tags = "Пользователи"
-    )
     @PatchMapping(path = "/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> updateUserImage(@RequestParam() MultipartFile image) {
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
 }
