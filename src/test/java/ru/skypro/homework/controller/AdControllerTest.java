de7868659b89421d2a24272d899b66039aa3d36c
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-08-13 01:38:14 +0300

    givenCreate_whenIcorrectInput_thenBadRequest edited

--- src/test/java/ru/skypro/homework/controller/AdControllerTest.java
+++ src/test/java/ru/skypro/homework/controller/AdControllerTest.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.skypro.homework.dto.CreateOrUpdateAd;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AdController.class)
@@ -26,16 +26,30 @@ class AdControllerTest {
     void givenCreate_whenIncorrectInput_thenBadRequest() throws Exception {
 
         CreateOrUpdateAd invalidCreateAd = new CreateOrUpdateAd();
-        invalidCreateAd.setDescription("123");
-        invalidCreateAd.setTitle("1");
+        invalidCreateAd.setDescription("---");
+        invalidCreateAd.setTitle("-");
         invalidCreateAd.setPrice(-1);
+        byte [] propertiesJson = objectMapper.writeValueAsBytes(invalidCreateAd);
 
-        String body = objectMapper.writeValueAsString(invalidCreateAd);
 
-        MockMultipartFile mockImgFile = new MockMultipartFile("mocked-img-file", new byte[0]);
+        MockMultipartFile propertiesMockMultipartFile = new MockMultipartFile("properties", "ad.txt",
+                "application/json", propertiesJson);
+        MockMultipartFile imageMockMultipartFile = new MockMultipartFile("image", "some-image.png",
+                "image/png", "an-image".getBytes());
 
-        mockMvc.perform(multipart("/ads", body, mockImgFile))
-                .andExpect(status().isBadRequest());
+        MockMultipartHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders
+                        .multipart("/ads");
+        builder.with(
+                request -> {
+                    request.setMethod("POST");
+                    return request;
+                });
+        builder.file(propertiesMockMultipartFile);
+        builder.file(imageMockMultipartFile);
+
+        mockMvc.perform(builder)
+          .andExpect(status().isBadRequest());
     }
 
     @Test
