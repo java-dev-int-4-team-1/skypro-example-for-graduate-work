6fa4de055cd98ce2d7126a021e90e2f642d2d974
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-08-18 19:19:38 +0300

    util.ImageManager added

--- src/main/java/ru/skypro/homework/service/AdService.java
+++ src/main/java/ru/skypro/homework/service/AdService.java
@@ -37,13 +37,28 @@ public class AdService {
         return adMapper.adsToAdsDto(Collections.emptyList());
     }
 
+    /**
+     * throws AdNotFoundException if there is no ad entry with the id in the db 
+     */
+    private Ad getAd(Integer id) {
+        return adRepository.findById(id)
+                .orElseThrow(() -> new AdNotFoundException(id)
+                );
+    }
+
+    private void uploadImg(Ad ad, MultipartFile image) {
+        try {
+            imageManager.uploadAdImg(ad, image);
+        } catch (IOException e)  {
+            log.error("uploadImg({}, image.name={}): IOException was thrown",
+                    ad, image.getName(), e
+            );
+        }
+    }
+
     public AdDto getById(Integer id) {
         log.debug("getById({})", id);
-        return adMapper.adToAdDto(
-                adRepository.findById(id)
-                        .orElseThrow(() -> new AdNotFoundException(id)
-                        )
-        );
+        return adMapper.adToAdDto(getAd(id));
     }
 
     public AdDto create(CreateOrUpdateAd properties, MultipartFile image) {
@@ -53,33 +68,32 @@ public class AdService {
         Ad ad = adRepository.save(
                 adMapper.createOrUpdateAdToAd(properties, image.getName())
         );
-        try {
-            imageManager.uploadAdImg(ad, image);
-        } catch (IOException e)  {
-            log.error("create({}, image.name={}): IOException was in ImageManager.uploadImage() thrown",
-                    properties, image.getName(), e
-            );
-        }
+        uploadImg(ad, image);
 
         return adMapper.adToAdDto(ad);
     }
 
-    /**
-     *
-     * @return true if the entry with the specified id was listed in the db, otherwise - false.
-     */
-    public boolean delete(Integer id) {
-        log.debug("create({})", id);
-        return false;
+    public void delete(int id) {
+        log.debug("delete({})", id);
+
+        adRepository.delete(getAd(id));
     }
 
-    public AdDto patchProperties(Integer id, CreateOrUpdateAd updateAd) {
-        log.debug("patch({}, {})", id, updateAd);
+    public AdDto patchProperties(int id, CreateOrUpdateAd updateAd) {
+        log.debug("patchProperties({}, {})", id, updateAd);
+
+        Ad ad = getAd(id);
+        updateAd.updateAd(ad);
+        adRepository.save(ad);        
         return adMapper.adToAdDto(new Ad());
     }
 
-    public AdDto patchImage(Integer id, MultipartFile image) {
-        log.debug("patch({}, {})", id, image);
+    public AdDto patchImage(int id, MultipartFile image) {
+        log.debug("patchImage({}, {})", id, image);
+        Ad ad = getAd(id);
+        ad.setImage(image.getName());
+        uploadImg(ad, image);
+        adRepository.save(ad);
         return adMapper.adToAdDto(new Ad());
     }
 
