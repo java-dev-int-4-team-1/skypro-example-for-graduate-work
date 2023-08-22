dd70f06653551b243f0c1c9f1271fe84e3e228bf
Stanislav <stdu92@mail.ru>
2023-08-23 02:22:29 +0300

    Correction UserMapper

--- src/main/java/ru/skypro/homework/mapper/UserMapper.java
+++ src/main/java/ru/skypro/homework/mapper/UserMapper.java
@@ -12,29 +12,16 @@ import ru.skypro.homework.entity.User;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "email", target = "email")
-    @Mapping(source = "firstName", target = "firstName")
-    @Mapping(source = "lastName", target = "lastName")
-    @Mapping(source = "phone", target = "phone")
-    @Mapping(source = "role", target = "role")
-    @Mapping(source = "image", target = "image")
+
     UserDto userEntityToUserDTO(User currentUser);
 
     @Mapping(source = "newPassword", target = "password")
     void updateNewPassword(NewPassword newPassword, @MappingTarget User currentUser);
 
-    @Mapping(source = "firstName", target = "firstName")
-    @Mapping(source = "lastName", target = "lastName")
-    @Mapping(source = "phone", target = "phone")
+
     void updateUser(UpdateUser updateUser, @MappingTarget User currentUser);
 
     @Mapping(source = "username", target = "email")
-    @Mapping(source = "password", target = "password")
-    @Mapping(source = "firstName", target = "firstName")
-    @Mapping(source = "lastName", target = "lastName")
-    @Mapping(source = "phone", target = "phone")
-    @Mapping(source = "role", target = "role")
     User registerFromRegisterDto(Register register);
 
     @Mapping(target = "image")
