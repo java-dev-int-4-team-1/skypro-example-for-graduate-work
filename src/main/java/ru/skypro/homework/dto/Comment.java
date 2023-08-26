3f788654b6636bc5835ae1f2e64ee81f1decb467
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-08-27 01:42:27 +0300

    Comment: Controller/Service/Repository/Dto/Entity/Mapper added

--- src/main/java/ru/skypro/homework/dto/Comment.java
+++ src/main/java/ru/skypro/homework/dto/Comment.java
@@ -7,7 +7,7 @@ public class Comment {
     private Integer author;
     private String authorImage;
     private String authorFirstName;
-    private Long createdAt;
+    private long createdAt;
     private Integer pk;
     private String text;
 }
