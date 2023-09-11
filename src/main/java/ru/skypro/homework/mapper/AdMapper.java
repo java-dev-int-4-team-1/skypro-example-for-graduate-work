f1cf22c87e770bae7cbecc509b9efaa77b5518e7
Sergei Gots <Serge.Gots@gmail.com>
2023-09-11 19:34:30 +0300

    ImageMapping code optimized

--- src/main/java/ru/skypro/homework/mapper/AdMapper.java
+++ src/main/java/ru/skypro/homework/mapper/AdMapper.java
@@ -3,7 +3,6 @@ package ru.skypro.homework.mapper;
 import lombok.RequiredArgsConstructor;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.AdDto;
 import ru.skypro.homework.dto.Ads;
@@ -19,13 +18,6 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public abstract class AdMapper extends ImageMapper {
 
-    @Value("${realm.img}")
-    private String realmImg;
-
-    @Value("${realm.ads}")
-    private String realmAds;
-
-
     public int map(User user) {
 
         if (user == null) {
@@ -39,11 +31,11 @@ public abstract class AdMapper extends ImageMapper {
     }
 
     public String mapImage (Ad ad) {
-        return buildImageMapping(realmImg, realmAds, ad);
+        return buildImageMapping(realmAds, ad);
     }
 
     @Mapping(source="id", target = "pk")
-    @Mapping(target = "image",  expression = "java( mapImage(ad) )")
+    @Mapping(target = "image",  expression = "java(mapImage(ad))")
     public abstract AdDto map(Ad ad);
 
     @Mapping(target = "authorFirstName", source="author.firstName")
--- src/main/java/ru/skypro/homework/mapper/CommentMapper.java
+++ src/main/java/ru/skypro/homework/mapper/CommentMapper.java
@@ -2,7 +2,6 @@ package ru.skypro.homework.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Value;
 import ru.skypro.homework.dto.CommentDto;
 import ru.skypro.homework.dto.Comments;
 import ru.skypro.homework.dto.CreateOrUpdateComment;
@@ -14,22 +13,17 @@ import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public abstract class CommentMapper extends ImageMapper {
-    @Value("${realm.img}")
-    private String realmImg;
-
-    @Value("${realm.users}")
-    private String realmUsers;
 
     public abstract Comment map(CreateOrUpdateComment createOrUpdateComment);
 
     @Mapping(source="id", target="pk")
     @Mapping(source="author.id", target="author")
     @Mapping(source="author.firstName", target="authorFirstName")
-    @Mapping(expression="java( mapImage(comment.getAuthor()) )", target="authorImage")
+    @Mapping(expression="java(mapImage(comment.getAuthor()))", target="authorImage")
     public abstract CommentDto map(Comment comment);
 
     public String mapImage(User author) {
-        return buildImageMapping(realmImg, realmUsers, author);
+        return buildImageMapping(realmUsers, author);
     }
 
     public Comments map(Collection<Comment> comments) {
