45b7fef62047551d12de8305eb6f0f37b881f15c
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-09-16 02:56:19 +0300

    unauthenticated user can view ads preview list: WebSecurityConfig WHITE_LIST edited

--- src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
+++ src/main/java/ru/skypro/homework/config/WebSecurityConfig.java
@@ -20,7 +20,8 @@ public class WebSecurityConfig {
             "/v3/api-docs",
             "/webjars/**",
             "/login",
-            "/register"
+            "/register",
+            "/ads"
     };
 
     @Bean
